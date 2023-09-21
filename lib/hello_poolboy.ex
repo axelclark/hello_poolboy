b2053ef6bff0d6e0cae2b96569283a60c88532e3
Axel Clark <axel.clark@pm.me>
2023-09-20 20:43:59 -0700

    Add basic poolboy setup

--- lib/hello_poolboy.ex
+++ lib/hello_poolboy.ex
@@ -1,18 +1,32 @@
 defmodule HelloPoolboy do
-  @moduledoc """
-  Documentation for `HelloPoolboy`.
-  """
+  @timeout 60000
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> HelloPoolboy.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def start do
+    1..20
+    |> Enum.map(fn i -> async_call_square_root(i) end)
+    |> Enum.each(fn task -> await_and_inspect(task) end)
   end
+
+  defp async_call_square_root(i) do
+    Task.async(fn ->
+      :poolboy.transaction(
+        :worker,
+        fn pid ->
+          # Let's wrap the genserver call in a try - catch block. This allows us to trap any exceptions
+          # that might be thrown and return the worker back to poolboy in a clean manner. It also allows
+          # the programmer to retrieve the error and potentially fix it.
+          try do
+            HelloPoolboy.Worker.square_root(pid, i)
+          catch
+            e, r ->
+              IO.inspect("poolboy transaction caught error: #{inspect(e)}, #{inspect(r)}")
+              :ok
+          end
+        end,
+        @timeout
+      )
+    end)
+  end
+
+  defp await_and_inspect(task), do: task |> Task.await(@timeout) |> IO.inspect()
 end
--- lib/hello_poolboy/application.ex
+++ lib/hello_poolboy/application.ex
@@ -8,13 +8,21 @@ defmodule HelloPoolboy.Application do
   @impl true
   def start(_type, _args) do
     children = [
-      # Starts a worker by calling: HelloPoolboy.Worker.start_link(arg)
-      # {HelloPoolboy.Worker, arg}
+      :poolboy.child_spec(:worker, poolboy_config())
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
     opts = [strategy: :one_for_one, name: HelloPoolboy.Supervisor]
     Supervisor.start_link(children, opts)
+  end
+
+  defp poolboy_config do
+    [
+      name: {:local, :worker},
+      worker_module: HelloPoolboy.Worker,
+      size: 3,
+      max_overflow: 0
+    ]
   end
 end
